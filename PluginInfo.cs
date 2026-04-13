b0e8bedb31316fb0f26b0bcd758debc3ce596933
Petar <57235791+xamionex@users.noreply.github.com>
2026-04-13 02:14:23 +0200

    Update version

--- PluginInfo.cs
+++ PluginInfo.cs
@@ -1,5 +1,5 @@
 internal static class PluginInfo {
 public const string PluginGuid = "amione.SaS2IndicatorsColorChanger";
 public const string PluginName = "SaS2IndicatorsColorChanger";
-public const string PluginVersion = "1.0.2";
+public const string PluginVersion = "1.0.3";
 }
